d472f4d96911d04c50b1e15015eb750faf786240
mehran-at <ajaxcube@gmail.com>
2019-01-15 16:31:26 +0100

    first commit

---
+++ src/Clubber.java
@@ -0,0 +1,27 @@
+public class Clubber {
+
+    private String name;
+    private Integer age;
+    private Integer money;
+
+    public Clubber(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Integer getMoney() {
+        return money;
+    }
+
+    public void setMoney(Integer money) {
+        this.money = money;
+    }
+}
